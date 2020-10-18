a332acc33aaa5e558f2a1fcede41ade3e66388b9
avacallery <avacallery@gmail.com>
2020-10-18 19:31:35 -0400

    create getters and setters for hourlyRate and baseSalary

--- src/com/avacallery/Employee.java
+++ src/com/avacallery/Employee.java
@@ -4,12 +4,31 @@ package com.avacallery;
 
 public class Employee {
     //fields are the blueprints to our objects
-    public int baseSalary;
-    public int hourlyRate;
+    private int baseSalary;
+    private int hourlyRate;
 
     // no need to add all parameters because we are storing the fields above
     public int calculateWage(int extraHours) {
         return baseSalary + (hourlyRate * extraHours);
     }
 
+    public void setBaseSalary(int baseSalary) {
+        if (baseSalary <= 0)
+            throw new IllegalArgumentException("Salary cannot be 0 or negative.");
+        this.baseSalary= baseSalary;
+    }
+
+    public int getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate) {
+        if (hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly rate cannot be 0 or negative.");
+        this.hourlyRate = hourlyRate;
+    }
+
+    public int getHourlyRate() {
+        return hourlyRate;
+    }
 }
